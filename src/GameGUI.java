4978f9dd5f641d8f71ee1d1f188f43dfeb510eff
Fabian <fabian.paul.gurtner@gmail.com>
2019-02-09 15:38:52 +0100

    Initial commit

---
+++ src/GameGUI.java
@@ -0,0 +1,27 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class GameGUI {
+	private JFrame frame = new JFrame("Breakout");
+
+
+	public GameGUI() {
+		frame.setLayout(null);
+	}
+
+	private class ImagePanel extends JPanel {
+		private BufferedImage image;
+
+		public ImagePanel(String filePath) {
+			try {
+				image = ImageIO.read(getClass().getResourceAsStream(filePath));
+			} catch(IOException e) {
+				System.err.println("Couldn't load image " + filePath + "!");
+			}
+		}
+
+
+	}
+}
